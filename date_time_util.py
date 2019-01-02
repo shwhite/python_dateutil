dbf9ec8597ffb870b53cc0cecaddf6acebce74c9
shwhite <wwhite103@gmail.com>
2019-01-02 14:48:59 +0900

    Update date_time_util.py
    띄어쓰기 수정

--- date_time_util.py
+++ date_time_util.py
@@ -1,9 +1,9 @@
 import datetime as dt
 
 def gettime_gap():
-  m_dt = dt.datetime.now()
-  gap_dt = dt.timedelta(minutes=10)
-  result_dt = m_dt - gap_dt
-  strtime = result_dt.strftime("%Y-%m-%d %H:%M:%S")
-  print(strtime)
+    m_dt = dt.datetime.now()
+    gap_dt = dt.timedelta(minutes=10)
+    result_dt = m_dt - gap_dt
+    strtime = result_dt.strftime("%Y-%m-%d %H:%M:%S")
+    print(strtime)
   
